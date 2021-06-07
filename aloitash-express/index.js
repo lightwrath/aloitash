e71cef0f8fc691267611454b6d772829538bd258
lightwrath <lightwrath@live.com>
2021-06-07 23:09:30 +0300

    move to server index

---
+++ aloitash-express/index.js
@@ -0,0 +1,102 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+const debug = require('debug')('aloitash-express:server');
+const http = require('http');
+
+
+var app = express();
+initExpressFeatures(app)
+initExpressRoutes(app)
+
+const port = normalizePort(process.env.PORT || '80');
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function initExpressFeatures(app) {
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, '../aloitash-client/build')));
+  app.use(cors())
+}
+
+function initExpressRoutes(app) {
+  const indexRouter = require('./routes/index')
+  const scriptRouter = require('./routes/script')
+  const runtimeRouter = require('./routes/runtime')
+  const terminalRouter = require('./routes/terminal')
+
+  app.use('/', indexRouter)
+  app.use('/script', scriptRouter)
+  app.use('/runtime', runtimeRouter)
+  app.use('/terminal', terminalRouter)
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err });
+  });
+}
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
